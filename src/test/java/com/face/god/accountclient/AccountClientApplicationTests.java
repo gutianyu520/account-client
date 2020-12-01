c96b7688c8c2f852b26bb817c5f4b3879291b3bf
KT104968 <kt104968@kentaku.co.jp>
2020-12-01 15:47:00 +0900

    first commit

---
+++ src/test/java/com/face/god/accountclient/AccountClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.face.god.accountclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
