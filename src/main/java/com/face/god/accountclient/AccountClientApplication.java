660714aa51f4401a2ee97190b92cd9722352461f
KT104968 <kt104968@kentaku.co.jp>
2020-12-01 16:09:30 +0900

    web添加

--- src/main/java/com/face/god/accountclient/AccountClientApplication.java
+++ src/main/java/com/face/god/accountclient/AccountClientApplication.java
@@ -2,8 +2,10 @@ package com.face.god.accountclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AccountClientApplication {
 
     public static void main(String[] args) {
